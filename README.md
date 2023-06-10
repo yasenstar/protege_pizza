77e9ae21c7af5432c85059ef6414d30eb5b04dba
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-06-10 17:02:29 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# protege_pizza
+# Ontology: Pizza.owl using Protégé
+
+Practice building pizza.owl ontology using Protege modeling tool
+
+## Ontologies inside Semantics Concept
+
+![ontology-clipart](img/ontology.png)
+
+Source from post of [Malcolm Chirholm Ph.D.](https://www.linkedin.com/in/malcolmchisholm?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAALGOX0Byp8PZaGgklIEBK0daX9A9WtjIQM&lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BYN15qj%2FISRyjIwyAgQtkjQ%3D%3D)
+
+| Terms          | Description                                                                                                         |
+| -------------- | ------------------------------------------------------------------------------------------------------------------- |
+| Ontologies     | Each is a "view" of the business world (and business information) that is required to meet a specific business need |
+| Business Rules | Atomic units of logic that govern behavior of concepts and relationships                                            |
+| Relationships  | Other business relationships outside taxonomies and hierarchies                                                     |
+| Hierarchies    | How individual things (instances) are associated at multiple levels for specific businees needs                     |
+| Taxonimies     | The relationships of general with specific concepts                                                                 |
+| Definitions    | Descriptions of the concepts identified by the terms                                                                |
+| Terms          | The specialized terms used in the business, with homonyms, synonyms, and universes of discourse identified          |
+
+## Learn Ontologies from pizza.owl
+
+Thanks for Michael DeBellis's great tutorial on pizza.owl, which is helping to understand the key concepts of ontology and also a way of building practices.
+
+This repository is tracking my hands-on modeling the pizza.owl in step-by-step manner, and you can find the snapshot ontology model files in every step so that you can start your practice from any point.
+
+## Why Protégé?
+
+![protege-logo](img/protege_logo.png)
+
+Check [Protege Website](https://protege.stanford.edu/), you can find below statements:
+
+*Protégé’s plug-in architecture can be adapted to build both simple and complex ontology-based applications. Developers can integrate the output of Protégé with rule systems or other problem solvers to construct a wide range of intelligent systems. Most important, the Stanford team and the vast Protégé community are here to help.*
+
+I'm using Protégé to learn pizza.owl and also practice the modeling skill here.
+
+Some reference quick link about Protégé:
+
+- [WebProtégé](http://webprotege.stanford.edu/)
+- [Download Windows Version 5.6.1](https://protege.stanford.edu/software.php), also have Java package for platform independent installating.
+- [Protégé wiki](https://protegewiki.stanford.edu/wiki/Main_Page)
+- [Protégé in Github](https://github.com/protegeproject), from here you can get latest version
+
+## Structure of pizza.owl
+
+Refer to the mindmap here:
+
+![mindmap](img/pizza_tutorial.jpg)
+
+---
+
+Hope you can get nice experience thorugh following this journey together with me, any questions are welcome to [reach_me](mailto:xiaoqizhao@outlook.com?subject=Protege-pizza.owl), thank you!  (2023)
---
+++ pizza.owl tutorial.mm
@@ -0,0 +1,194 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="pizza.owl tutorial" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1686323667754" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#fbf1c7" zoom="0.75">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/light_gruvbox_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_7974122" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_7974122" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#cc241d" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc241d"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_103960811" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc241d" TRANSPARENCY="255" DESTINATION="ID_103960811"/>
+<font SIZE="12" ITALIC="false"/>
+<edge COLOR="#cc241d"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#fdf6e3" BACKGROUND_COLOR="#282828" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#282828" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#282828" BACKGROUND_COLOR="#ebdbb2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#282828" BACKGROUND_COLOR="#d79921" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#458588" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#ffffff" BACKGROUND_COLOR="#b16286" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#ffffff" BACKGROUND_COLOR="#689d6a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#ffffff" BACKGROUND_COLOR="#a89984" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#ebdbb2" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#ebdbb2">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#98971a">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="ch01 introduction" POSITION="right" ID="ID_1844456199" CREATED="1686323713926" MODIFIED="1686323728609"/>
+<node TEXT="ch02 requirements and the Protege UI" POSITION="right" ID="ID_320917976" CREATED="1686323729358" MODIFIED="1686323741570"/>
+<node TEXT="ch03 OWL ontologies overview" POSITION="right" ID="ID_1964726528" CREATED="1686323750103" MODIFIED="1686323763306">
+<node TEXT="3.1 components of OWL ontologies" ID="ID_1938527352" CREATED="1686330866704" MODIFIED="1686330877389">
+<node TEXT="3.1.1 individuals" ID="ID_242879608" CREATED="1686330883000" MODIFIED="1686330891798"/>
+<node TEXT="3.1.2 properties" ID="ID_600190457" CREATED="1686330892235" MODIFIED="1686330907817"/>
+<node TEXT="3.1.3 classes" ID="ID_1649031439" CREATED="1686330908388" MODIFIED="1686330914137"/>
+</node>
+</node>
+<node TEXT="ch04 building an OWL ontology" POSITION="right" ID="ID_808215330" CREATED="1686323769070" MODIFIED="1686323779883">
+<node TEXT="ex01: create a new OWL ontology" ID="ID_729877616" CREATED="1686330923734" MODIFIED="1686330940733"/>
+<node TEXT="ex02: set the preferences for new entities and rendering" ID="ID_1912835714" CREATED="1686330945549" MODIFIED="1686330957927"/>
+<node TEXT="ex03: add a comment annotation to ontology" ID="ID_1520449047" CREATED="1686330964461" MODIFIED="1686330974429"/>
+<node TEXT="4.1 named classes" ID="ID_366493979" CREATED="1686330979356" MODIFIED="1686330983323">
+<node TEXT="ex04: create classes: Pizza, PizzaTopping, and PizzaBase" ID="ID_1334136171" CREATED="1686330987574" MODIFIED="1686331006718"/>
+</node>
+<node TEXT="4.2 using a reasoner" ID="ID_1031072502" CREATED="1686331011882" MODIFIED="1686331017637">
+<node TEXT="ex05: install and run the Pellet Reasoner" ID="ID_151798472" CREATED="1686331020874" MODIFIED="1686331046172"/>
+</node>
+<node TEXT="4.3 disjoint classes" ID="ID_1968698660" CREATED="1686331050792" MODIFIED="1686331055439">
+<node TEXT="ex06: make Pizza, PizzaTopping, and PizzaBase disjoint from each other" ID="ID_1640827721" CREATED="1686331057366" MODIFIED="1686331079957"/>
+</node>
+<node TEXT="4.4 using create class hierarchy" ID="ID_175386" CREATED="1686331086241" MODIFIED="1686331095398">
+<node TEXT="ex07: use the create class hierarchy tool to create subclasses of PizzaBase" ID="ID_554269383" CREATED="1686331098050" MODIFIED="1686331115651"/>
+</node>
+<node TEXT="4.5 create a PizzaTopping hierarchy" ID="ID_1055735125" CREATED="1686331119631" MODIFIED="1686331137479">
+<node TEXT="ex08: create subclasses of PizzaTopping" ID="ID_1589422046" CREATED="1686331138159" MODIFIED="1686331149032"/>
+</node>
+<node TEXT="4.6 OWL porperties" ID="ID_158334547" CREATED="1686331171320" MODIFIED="1686331178109">
+<node TEXT="ex09: create some properties" ID="ID_523616874" CREATED="1686331181275" MODIFIED="1686331189755"/>
+</node>
+<node TEXT="4.7 inverse properties" ID="ID_240358113" CREATED="1686331193936" MODIFIED="1686331199984">
+<node TEXT="ex10: create some inverse properties" ID="ID_891087200" CREATED="1686331201354" MODIFIED="1686331209276"/>
+</node>
+<node TEXT="4.8 OWL object property characteristics" ID="ID_1497253479" CREATED="1686331215729" MODIFIED="1686331228476">
+<node TEXT="4.8.1 functional properties" ID="ID_1668752960" CREATED="1686331228859" MODIFIED="1686331235412"/>
+<node TEXT="4.8.2 inverse functional properties" ID="ID_883388361" CREATED="1686331241837" MODIFIED="1686331249459"/>
+<node TEXT="4.8.3 transitive properties" ID="ID_1028302633" CREATED="1686331249686" MODIFIED="1686331255694"/>
+<node TEXT="4.8.4 symmetric and asymmetric properties" ID="ID_1558167378" CREATED="1686331260656" MODIFIED="1686331270517"/>
+<node TEXT="4.8.5 reflexive and irreflexive properties" ID="ID_1242075338" CREATED="1686331273961" MODIFIED="1686331285378"/>
+<node TEXT="4.8.6 reasoners automatically enforce property characteristics" ID="ID_471162566" CREATED="1686331285991" MODIFIED="1686331300703"/>
+</node>
+<node TEXT="4.9 OWL property domains and ranges" ID="ID_1969737760" CREATED="1686331307610" MODIFIED="1686331317019">
+<node TEXT="ex11: define the domain and range of the hasTopping property" ID="ID_193688812" CREATED="1686331324729" MODIFIED="1686331340541"/>
+<node TEXT="ex12: define the domain and range for the hasBase property" ID="ID_42082905" CREATED="1686331347788" MODIFIED="1686331361150"/>
+</node>
+<node TEXT="4.10 describing and defining classes" ID="ID_1412597679" CREATED="1686331364559" MODIFIED="1686331372953">
+<node TEXT="4.10.1 property restrictions" ID="ID_1272994921" CREATED="1686331376089" MODIFIED="1686331383336"/>
+<node TEXT="4.10.2 existential restrictions" ID="ID_1943188364" CREATED="1686331389974" MODIFIED="1686331397183">
+<node TEXT="ex13: add a restriction to Pizza that specifies a Pizza must have a PizzaBase" ID="ID_255959855" CREATED="1686331399386" MODIFIED="1686331423008"/>
+<node TEXT="ex14: create subclasses of Pizza: NamedPizza and MargheritaPizza" ID="ID_1017161283" CREATED="1686331430495" MODIFIED="1686331450319"/>
+<node TEXT="ex15: create restrictions that define a MargheritaPizza" ID="ID_870541573" CREATED="1686331454788" MODIFIED="1686331468114"/>
+<node TEXT="ex16: create AmericanaPizza by cloning MargheritaPizza and adding additional restrictions" ID="ID_1914041000" CREATED="1686331472917" MODIFIED="1686331498073"/>
+<node TEXT="ex17: create AmericanaHotPizza and SohoPizza" ID="ID_864889418" CREATED="1686331501568" MODIFIED="1686331513362"/>
+<node TEXT="ex18: make subclasses of NamedPizza disjoint" ID="ID_1087079419" CREATED="1686331515887" MODIFIED="1686331527960"/>
+<node TEXT="ex19: add a probe class called ProbeInconsistentTopping" ID="ID_1505382137" CREATED="1686331532103" MODIFIED="1686331548161"/>
+</node>
+</node>
+<node TEXT="4.11 primitive and defined classes (necessary and sufficient axioms)" ID="ID_1915518251" CREATED="1686331555686" MODIFIED="1686331573859">
+<node TEXT="ex20: create the CheesyPizza class" ID="ID_1287449845" CREATED="1686331574234" MODIFIED="1686331585321"/>
+<node TEXT="ex21: convert CheesyPizza from a primitive class to a defined class" ID="ID_554917188" CREATED="1686331589182" MODIFIED="1686331615225"/>
+</node>
+<node TEXT="4.12 universal restrictions" ID="ID_1241831728" CREATED="1686331620880" MODIFIED="1686331629430">
+<node TEXT="ex22: create a defined class called VegetarianPizza" ID="ID_1675219662" CREATED="1686331632073" MODIFIED="1686331646778"/>
+</node>
+<node TEXT="4.13 automated classification and open world reasoning" ID="ID_1060965124" CREATED="1686331651773" MODIFIED="1686331664001">
+<node TEXT="ex23: add a closure axiom on the hasTopping property for MargheritaPizza" ID="ID_450929778" CREATED="1686331667248" MODIFIED="1686331686985"/>
+</node>
+<node TEXT="4.14 defining an enumerated class" ID="ID_334001869" CREATED="1686331692538" MODIFIED="1686331701281">
+<node TEXT="ex24: create an enumerated class to represent the Spiciness of a Pizza" ID="ID_1480564502" CREATED="1686331704114" MODIFIED="1686331720639"/>
+</node>
+<node TEXT="4.15 adding Spiciness as a Property" ID="ID_130501377" CREATED="1686331724729" MODIFIED="1686331735088">
+<node TEXT="ex25: create and use the hasSpiciness property" ID="ID_904593240" CREATED="1686331738173" MODIFIED="1686331754056"/>
+<node TEXT="ex26: create an InterestingPizza that has at least three toppings" ID="ID_598929433" CREATED="1686331759685" MODIFIED="1686331775619"/>
+</node>
+</node>
+<node TEXT="ch05 datatype proterties" POSITION="right" ID="ID_1703657255" CREATED="1686323780054" MODIFIED="1686323797178">
+<node TEXT="5.1 defining a data property" ID="ID_1669475927" CREATED="1686331783144" MODIFIED="1686331791735">
+<node TEXT="ex27: create a datatype property called hasCaloricContent" ID="ID_1579530266" CREATED="1686331796097" MODIFIED="1686331813439"/>
+</node>
+<node TEXT="5.2 customizing the Protege user interface" ID="ID_538351183" CREATED="1686331820185" MODIFIED="1686331830754">
+<node TEXT="ex28: create example Pizza individuals" ID="ID_952564360" CREATED="1686331838050" MODIFIED="1686331850655"/>
+<node TEXT="ex29: assign a data property values" ID="ID_151580246" CREATED="1686331854617" MODIFIED="1686331862911"/>
+<node TEXT="ex30: create more instances and data property values" ID="ID_946225042" CREATED="1686334993026" MODIFIED="1686335042288"/>
+<node TEXT="ex31: create a datatype restriction that every Pizza hasCaloricContent" ID="ID_870660298" CREATED="1686335044021" MODIFIED="1686335081763"/>
+<node TEXT="ex32: create a HighCaloriePizza defined class" ID="ID_1932251002" CREATED="1686335085843" MODIFIED="1686335099729"/>
+</node>
+</node>
+<node TEXT="ch06 adding order to an enumerated class" POSITION="right" ID="ID_1312614168" CREATED="1686323799302" MODIFIED="1686323809888"/>
+<node TEXT="ch07 names: IRI&apos;s, labels, and namespaces" POSITION="right" ID="ID_416574278" CREATED="1686323810349" MODIFIED="1686323826817"/>
+<node TEXT="ch08 a larger ontology with some individuals" POSITION="right" ID="ID_1111048405" CREATED="1686323836071" MODIFIED="1686323849806"/>
+<node TEXT="ch09 queries: description logic and SPARQL" POSITION="right" ID="ID_1173967935" CREATED="1686323850221" MODIFIED="1686323862625">
+<node TEXT="9.1: description logic queries" ID="ID_240243003" CREATED="1686335129852" MODIFIED="1686335194717">
+<node TEXT="ex33: try some description logic queries" ID="ID_843348068" CREATED="1686335157891" MODIFIED="1686335181607"/>
+</node>
+<node TEXT="9.2 SPARQL queries" ID="ID_304792510" CREATED="1686335198987" MODIFIED="1686335208214">
+<node TEXT="9.2.1 some SPARQL Pizza queries" ID="ID_1204980415" CREATED="1686335228258" MODIFIED="1686335245038"/>
+<node TEXT="9.2.2 SPARQL and IRI names" ID="ID_1532426756" CREATED="1686335280599" MODIFIED="1686335293733"/>
+</node>
+</node>
+<node TEXT="ch10 SWRL and SQWRL" POSITION="right" ID="ID_1982574526" CREATED="1686323863341" MODIFIED="1686323883176">
+<node TEXT="ex34: write your first SWRL rule" ID="ID_475355367" CREATED="1686335307772" MODIFIED="1686335320629"/>
+<node TEXT="ex35: write another SWRL rule" ID="ID_930780383" CREATED="1686335325704" MODIFIED="1686335334705"/>
+<node TEXT="ex36: write a SQWRL rule" ID="ID_640330184" CREATED="1686335338528" MODIFIED="1686335347420"/>
+</node>
+<node TEXT="ch11 SHACL" POSITION="right" ID="ID_133290783" CREATED="1686323883701" MODIFIED="1686323888771">
+<node TEXT="11.1 OWA and monotonic reasoning" ID="ID_1163697728" CREATED="1686335356812" MODIFIED="1686335367808"/>
+<node TEXT="11.2 the real world is messy" ID="ID_980974229" CREATED="1686335370575" MODIFIED="1686335377398"/>
+<node TEXT="11.3 basic SHACL concepts" ID="ID_781348311" CREATED="1686335379092" MODIFIED="1686335386649"/>
+<node TEXT="11.4 the Protege SHACL plug-in" ID="ID_854164933" CREATED="1686335389556" MODIFIED="1686335400343"/>
+</node>
+<node TEXT="ch12 Web Protege" POSITION="right" ID="ID_317981913" CREATED="1686325216634" MODIFIED="1686325231920"/>
+<node TEXT="ch13 conclusions" POSITION="right" ID="ID_155728250" CREATED="1686325234262" MODIFIED="1686325243355"/>
+<node TEXT="ch14 bibliography" POSITION="right" ID="ID_90886172" CREATED="1686325243765" MODIFIED="1686325253840"/>
+</node>
+</map>
